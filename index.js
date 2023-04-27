69ec2d754b23979fedb5dde16d7500912de278ca
MarianaBazotto <marianasantosbazottok@gmail.com>
2023-04-27 08:53:20 -0300

    exercicios

---
+++ index.js
@@ -0,0 +1,47 @@
+// console.log("hello")
+// comentario
+//prompt("digite seu nome")
+
+// const idade = prompt("digite sua idade")
+// console.log(idade)
+// const endereco = prompt("digite seu endereco")
+// console.log(endereco)
+// const nome = prompt("digite seu nome")
+// console.log(nome)
+
+// const nome = prompt("digite seu nome")
+// const idade = prompt("digite sua idade")
+// console.log("ola!" , "meu nome é" ,nome, "e eu tenho" , idade, "anos")
+
+// const nome = prompt("digite seu nome")
+
+// const sobrenome = prompt("digite seu sobrenome")
+// const idade = prompt("digite sua idade")
+// const estudante = prompt("voce e estudante?")
+// console.log(nome, idade,  estudante)
+
+// const got = "Game of thrones"
+// const temporadas = 8
+// console.log(typeof got)
+// console.log(typeof temporadas)
+
+//exercicios
+
+let nome 
+let idade 
+console.log(typeof nome)
+console.log(typeof idade)
+
+nome = String(prompt("digite seu nome"))
+idade = Number(prompt("digite sua idade"))
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+console.log("Ola!" , "meu nome é" ,nome, "e eu tenho" ,idade, "anos")
+
+
+
+
+
+
